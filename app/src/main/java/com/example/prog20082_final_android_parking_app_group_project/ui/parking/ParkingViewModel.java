ca1ca292b4dbd9f4d743b156f732dba99b6e741d
joelgnanasekaram <gnanasej@sheridancollege.ca>
2019-11-25 16:09:49 -0500

    Updated ParkingViewModel
    Updated ParkingViewModel: Moved the ParkViewModel  logic from the custom ParkingViewModel to the pre-built ParkingViewModel of the Parking App.

--- app/src/main/java/com/example/prog20082_final_android_parking_app_group_project/ui/parking/ParkingViewModel.java
+++ app/src/main/java/com/example/prog20082_final_android_parking_app_group_project/ui/parking/ParkingViewModel.java
@@ -1,15 +1,45 @@
 package com.example.prog20082_final_android_parking_app_group_project.ui.parking;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class ParkingViewModel extends ViewModel {
+import com.example.prog20082_final_android_parking_app_group_project.db.ParkingRepository;
+import com.example.prog20082_final_android_parking_app_group_project.parking_model.Parking;
+
+import java.util.List;
+
+public class ParkingViewModel extends AndroidViewModel {
 
     private MutableLiveData<String> mText;
+    private LiveData<List<Parking>> allParking;
+    private ParkingRepository parkingRepository;
 
-    public ParkingViewModel() {
+
+    public ParkingViewModel(@NonNull Application application) {
+        super(application);
         mText = new MutableLiveData<>();
+        parkingRepository = new ParkingRepository(application);
+        allParking = parkingRepository.getAllParking();
+    }
+
+    public void insert(Parking parking){
+        parkingRepository.insert(parking);
+    }
+
+    public LiveData<List<Parking>> getAllParking(){
+        return allParking;
+    }
+
+    public void delete(Parking parking){
+        parkingRepository.delete(parking);
+    }
+
+    public void deleteAllParking(){
+        parkingRepository.deleteAllParking();
     }
 
     public LiveData<String> getText() {
