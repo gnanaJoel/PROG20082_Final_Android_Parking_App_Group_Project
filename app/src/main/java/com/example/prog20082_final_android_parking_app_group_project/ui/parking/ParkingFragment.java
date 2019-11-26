6eb2b179352aa21e7c5c6c0d95ca3d2dacc75c70
joelgnanasekaram <gnanasej@sheridancollege.ca>
2019-11-25 20:27:59 -0500

    Updated ViewModelProviders in Parking Fragment
    Updated the ViewModelProviders in the Parking Fragment of the Parking App to the latest ViewModelProvider constructor methods.

--- app/src/main/java/com/example/prog20082_final_android_parking_app_group_project/ui/parking/ParkingFragment.java
+++ app/src/main/java/com/example/prog20082_final_android_parking_app_group_project/ui/parking/ParkingFragment.java
@@ -14,9 +14,13 @@ import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.prog20082_final_android_parking_app_group_project.R;
+import com.example.prog20082_final_android_parking_app_group_project.parking_model.Parking;
+
+import java.util.List;
 
 public class ParkingFragment extends Fragment implements View.OnClickListener {
 
@@ -34,8 +38,7 @@ public class ParkingFragment extends Fragment implements View.OnClickListener {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        parkingViewModel =
-                ViewModelProviders.of(this).get(ParkingViewModel.class);
+        parkingViewModel = new ViewModelProvider(this).get(ParkingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_add_parking, container, false);
         final TextView textView = root.findViewById(R.id.tv_add_parking_title);
         parkingViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
